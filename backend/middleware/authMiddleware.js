691fc0901031dd373ae2a916421c9f177036c920
Vishwas Ahuja <vishwasahuja62@gmail.com>
2026-01-31 17:53:52 +0530

    Edited authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,30 +2,29 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const protect = async (req, res, next) => {
-  let token;
+  // Read token from Cookies instead of headers
+  const token = req.cookies.jwt;
 
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+  if(token){
     try {
-      token = req.headers.authorization.split(' ')[1];
-
+      // Verifying Token
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+      // Fetching User From Token
       req.user = await User.findById(decoded.id).select('-password');
-      
-      if (!req.user) {
-        return res.status(401).json({ message: 'Not authorized, user not found' });
-      }
 
+      if(!req.user){
+        return res.status(401).json({message : 'Not Authorized, User Not Found'});
+      }
 
       next();
     } catch (error) {
       console.error(error);
-      return res.status(401).json({ message: 'Not authorized, token failed' });
+      return res.status(401).json({message : 'Not Authorized, Token Failed'});
     }
-  }
-
-  if (!token) {
-    return res.status(401).json({ message: 'Not authorized, no token' });
+  } else {
+    // If No Token Is Found In Cookies
+    return res.status(401).json({message : 'Not Authorized, No Token'});
   }
 };
 
